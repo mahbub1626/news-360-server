16c880dc34ea3550be6fec54d6d22f3351137f28
mahbub1626 <mahbub1626@gmail.com>
2022-10-25 09:33:22 +0600

    category and news api worked

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.get('/news', (req, res)=>{
 
 app.get('/news/:id', (req, res) => {
     const id = req.params.id;
+    // console.log(id);
     const selectedNews = news.find(n => n._id === id);
     res.send(selectedNews);
 })
